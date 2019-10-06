491c48168714dac712faa8751fe89f5518d8f388
jorgemoreno <6826544+Jorg3Moreno@users.noreply.github.com>
2019-10-06 17:56:32 -0500

    Added Header component

---
+++ src/containers/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "../assets/styles/App.scss";
+import Header from "../components/Header";
+
+const App = () => {
+  <div className="app">
+    <Header />
+  </div>;
+};
+
+export default Header;
